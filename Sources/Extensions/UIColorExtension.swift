9e993da203901503ec061bc28f65117248ca0ff2
Milos Dancilovic <milos.dancilovic@avawomen.com>
2020-02-13 13:11:35 +0100

    init commit

---
+++ Sources/Extensions/UIColorExtension.swift
@@ -0,0 +1,44 @@
+//
+//  Extensions.swift
+//  SPMDemo
+//
+//  Created by Milos Dancilovic on 2/12/20.
+//  Copyright © 2020 com.avawomen.ios-ava-spm. All rights reserved.
+//
+
+import UIKit
+
+public extension UIColor {
+    static var avaLowFertility: UIColor {
+        return #colorLiteral(red: 0.3669729233, green: 0.7128614187, blue: 0.5819185376, alpha: 1)
+    }
+
+    static var avaLowFertilityPredicted: UIColor {
+        return #colorLiteral(red: 0.768627451, green: 0.8941176471, blue: 0.8235294118, alpha: 1)
+    }
+
+    static var avaLowFertilityMens: UIColor {
+        return #colorLiteral(red: 0.8156862745, green: 0.9176470588, blue: 0.8588235294, alpha: 1)
+    }
+
+    static var avaHighFertility: UIColor {
+        return #colorLiteral(red: 0.8751735091, green: 0.3557263017, blue: 0.4035576582, alpha: 1)
+    }
+
+    static var avaHighPredicted: UIColor {
+        return #colorLiteral(red: 1, green: 0.8666666667, blue: 0.8941176471, alpha: 1)
+    }
+
+    static var avaHighFertilityMens: UIColor {
+        return #colorLiteral(red: 1, green: 0.8941176471, blue: 0.9137254902, alpha: 1)
+    }
+
+    static var avaLearningFertilityPurple: UIColor {
+        return #colorLiteral(red: 0.3796435297, green: 0.3704194725, blue: 0.5486230254, alpha: 1)
+    }
+
+    static var avaCalendarBackground: UIColor {
+        return #colorLiteral(red: 0.9647058824, green: 0.9607843137, blue: 0.9882352941, alpha: 1)
+    }
+    
+}
